b7ca052c7fe04f18b3e0ab8e4668348f76c31e74
ElviraVargas7 <elviravarber@gmail.com>
2025-06-12 17:54:16 -0600

    adding cors middleware

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ from fastapi import FastAPI
 import models
 from db.setup import engine
 from routes import members, answers, questions
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # This is the next.js frontend ✨
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(members.router, prefix="/members", tags=["Members"])
 app.include_router(answers.router, prefix="/answers", tags=["Answers"])
